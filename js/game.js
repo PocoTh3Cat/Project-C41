80d334a2e51d71f2ec1bf0ad66720a68dbdcd00e
PocoTh3Cat <101481506+PocoTh3Cat@users.noreply.github.com>
2022-12-02 16:15:20 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -132,7 +132,7 @@ class Game{
                       }
 
                       if(obstacleGroup.isTouching(players)){
-                       gameState = 0;
+                       gameState = 2;
                        // gameState = 1;
                        // gameState = 2;
                        // gameState = 3;
